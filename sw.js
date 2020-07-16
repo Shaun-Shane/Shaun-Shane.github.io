6752a5e183bb98a2fbeef0275e5e237dc545be0b
Shaun-Shane <1953181@tongji.edu.cn>
2020-07-16 14:25:23 +0800

    Site updated: 2020-07-16 14:25:17

--- sw.js
+++ sw.js
@@ -24,15 +24,15 @@ var precacheUrls = [];
 
   precacheUrls.push('/');
 
-  precacheUrls.push('/2020/07/13/Best-Cow-Line-G/');
-
-  precacheUrls.push('/2020/07/15/Island/');
-
   precacheUrls.push('/2020/07/12/ALO/');
 
   precacheUrls.push('/2020/07/13/Array-Shrinking/');
 
-  precacheUrls.push('/2020/07/11/mo-ni-tui-huo/');
+  precacheUrls.push('/2020/07/13/Best-Cow-Line-G/');
+
+  precacheUrls.push('/2020/07/15/Island/');
+
+  precacheUrls.push('/2020/07/14/convex-hull/');
 
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":5};
