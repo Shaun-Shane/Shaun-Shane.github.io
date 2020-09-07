6338b81afba14e1587fbcb54dee771745d2896f6
Shaun-Shane <1953181@tongji.edu.cn>
2020-09-07 08:59:53 +0800

    Site updated: 2020-09-07 08:59:44

--- sw.js
+++ sw.js
@@ -26,11 +26,11 @@ var precacheUrls = [];
 
   precacheUrls.push('/2020/08/12/A-Twisty-Movement/');
 
+  precacheUrls.push('/2020/07/13/Array-Shrinking/');
+
   precacheUrls.push('/2020/07/12/ALO/');
 
   precacheUrls.push('/2020/08/11/Adaptive-Simpson/');
-
-  precacheUrls.push('/2020/07/13/Array-Shrinking/');
 
   precacheUrls.push('/2020/07/13/Best-Cow-Line-G/');
 
